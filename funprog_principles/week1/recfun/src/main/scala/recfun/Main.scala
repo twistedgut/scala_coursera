fe96bad2d835a9165e86fa824849d68aef84b519
John Maslen <j.maslen@net-a-porter.com>
2016-05-27 11:13:15 +0100

    First attempt at assignment

--- funprog_principles/week1/recfun/src/main/scala/recfun/Main.scala
+++ funprog_principles/week1/recfun/src/main/scala/recfun/Main.scala
@@ -13,13 +13,35 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
+    def pascal(c: Int, r: Int): Int = {
+      def loop (col: Int, row: Int): Int = {
+        if (col == 0 || col == row) 1
+        else loop(col-1, row-1) + loop(col, row-1)
+      }
+      loop(c, r)
+    }
   
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
-  
+    def balance(chars: List[Char]): Boolean = {
+      def loop(counter: Int, c: List[Char]): Boolean = {
+        if (counter < 0) false
+        else if (c.isEmpty)
+          if(counter == 0) true else false
+        else
+
+        {
+          val countParenth =
+            if (c.head == '(') counter + 1
+            else if (c.head == ')') counter - 1
+            else counter
+          loop(countParenth, c.tail)
+        }
+      }
+      loop(0, chars)
+    }
+
   /**
    * Exercise 3
    */
