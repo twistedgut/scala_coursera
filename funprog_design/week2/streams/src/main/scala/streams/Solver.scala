39f16200d86d734d9bc4d985523d9666012a484e
John Maslen <j.maslen@net-a-porter.com>
2016-07-24 21:40:43 +0100

    Work in progress

--- funprog_design/week2/streams/src/main/scala/streams/Solver.scala
+++ funprog_design/week2/streams/src/main/scala/streams/Solver.scala
@@ -28,7 +28,13 @@ trait Solver extends GameDef {
    * It should only return valid neighbors, i.e. block positions
    * that are inside the terrain.
    */
-  def neighborsWithHistory(b: Block, history: List[Move]): Stream[(Block, List[Move])] = ???
+  def neighborsWithHistory(b: Block, history: List[Move]): Stream[(Block, List[Move])] = {
+    for (
+      (block, move) <- b.legalNeighbors.toStream
+    ) yield (block, move :: history)
+
+    //b.legalNeighbors.toStream.map( b => (b._1, b._2 :: history))
+  }
 
   /**
    * This function returns the list of neighbors without the block
@@ -36,7 +42,14 @@ trait Solver extends GameDef {
    * make sure that we don't explore circular paths.
    */
   def newNeighborsOnly(neighbors: Stream[(Block, List[Move])],
-                       explored: Set[Block]): Stream[(Block, List[Move])] = ???
+                       explored: Set[Block]): Stream[(Block, List[Move])] = {
+    for (
+      (block, listMoves) <- neighbors if !explored(block)
+    ) yield (block, listMoves)
+
+    //neighbors.filterNot(bl => explored.contains(bl._1))
+
+  }
 
   /**
    * The function `from` returns the stream of all possible paths
