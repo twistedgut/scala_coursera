4cc63180bf7124bc0688986ffb38b79b59384039
John Maslen <john.maslen@ynap.com>
2016-08-11 15:49:08 +0100

    Part of week 4 assignment

--- funprog_design/week4/calculator/src/main/scala/calculator/TweetLength.scala
+++ funprog_design/week4/calculator/src/main/scala/calculator/TweetLength.scala
@@ -3,12 +3,16 @@ package calculator
 object TweetLength {
   final val MaxTweetLength = 140
 
-  def tweetRemainingCharsCount(tweetText: Signal[String]): Signal[Int] = {
-    ???
-  }
+  def tweetRemainingCharsCount(tweetText: Signal[String]): Signal[Int] = Signal(MaxTweetLength - tweetLength(tweetText()))
 
   def colorForRemainingCharsCount(remainingCharsCount: Signal[Int]): Signal[String] = {
-    ???
+    Signal{
+      remainingCharsCount() match {
+        case z if (z < 0)   => "red"
+        case z if (z < 15)  => "orange"
+        case _              => "green"
+      }
+    }
   }
 
   /** Computes the length of a tweet, given its text string.
