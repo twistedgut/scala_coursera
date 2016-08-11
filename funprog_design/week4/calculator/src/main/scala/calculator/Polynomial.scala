9c80fb6c407cc772c5b0f80c3520aef7b2bd8fcb
John Maslen <john.maslen@ynap.com>
2016-08-11 16:17:33 +0100

    Part of week 4 assignment

--- funprog_design/week4/calculator/src/main/scala/calculator/Polynomial.scala
+++ funprog_design/week4/calculator/src/main/scala/calculator/Polynomial.scala
@@ -1,13 +1,19 @@
 package calculator
 
 object Polynomial {
-  def computeDelta(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double]): Signal[Double] = {
-    ???
-  }
+  def computeDelta(a: Signal[Double], b: Signal[Double], c: Signal[Double]): Signal[Double] =
+    Signal { b() * b() - 4 * (a() * c()) }
 
-  def computeSolutions(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+  def computeSolutions(a: Signal[Double], b: Signal[Double], c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
+    Signal{
+      val bmo = b() * -1
+      val sqd = math.sqrt(delta())
+      val tta = 2 * a()
+      delta() match {
+        case z if z <  0.0 => Set()
+        case z if z == 0.0 => Set(bmo / tta)
+        case z if z >  0.0 => Set((bmo + sqd) / tta, (bmo - sqd) / tta)
+      }
+    }
   }
 }
