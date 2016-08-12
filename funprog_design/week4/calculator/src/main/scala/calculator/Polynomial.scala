42d61fa93dccf4be7a3d437122d3a5e813cbe835
John Maslen <john.maslen@ynap.com>
2016-08-12 12:53:34 +0100

    Initial attempt at calculator II

--- funprog_design/week4/calculator/src/main/scala/calculator/Polynomial.scala
+++ funprog_design/week4/calculator/src/main/scala/calculator/Polynomial.scala
@@ -2,10 +2,10 @@ package calculator
 
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double], c: Signal[Double]): Signal[Double] =
-    Signal { b() * b() - 4 * (a() * c()) }
+    new Signal { b() * b() - 4 * (a() * c()) }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double], c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    Signal{
+    new Signal{
       val bmo = b() * -1
       val sqd = math.sqrt(delta())
       val tta = 2 * a()
